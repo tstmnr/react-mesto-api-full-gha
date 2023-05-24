9a8fa2be54157a002c2e3e344b263dd9b14f6256
Eugeniy Razveev <razveev.evg@gmail.com>
2023-05-24 20:28:02 +0300

    fix cors

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -1,25 +1,27 @@
-const allowedCors = [
+const ALLOWED_CORS = [
   'https://tstmnr.nomoredomains.monster',
   'http://tstmnr.nomoredomains.monster',
+  'https://158.160.26.122',
+  'http://158.160.26.122',
+  'https://localhost:3000',
+  'http://localhost:3000',
 ];
 
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
+// CORS MIDDLEWARE
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
-
-  if (allowedCors.includes(origin)) {
+  if (ALLOWED_CORS.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Credentials', true);
   }
-
   if (method === 'OPTIONS') {
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
     res.header('Access-Control-Allow-Headers', requestHeaders);
-    res.end();
-    return;
+    return res.end();
   }
-
-  next();
+  return next();
 };
