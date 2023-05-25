d5df04a79acfdd67018474e4fe3df9f98f93bac1
Eugeniy Razveev <razveev.evg@gmail.com>
2023-05-25 20:16:45 +0300

    fix cors v2

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -10,7 +10,7 @@ const ALLOWED_CORS = [
 const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
 // CORS MIDDLEWARE
-module.exports = (req, res, next) => {
+/* module.exports = (req, res, next) => {
   const { origin } = req.headers;
   const { method } = req;
   const requestHeaders = req.headers['access-control-request-headers'];
@@ -24,4 +24,23 @@ module.exports = (req, res, next) => {
     return res.end();
   }
   return next();
+}; */
+
+// eslint-disable-next-line consistent-return
+module.exports = (req, res, next) => {
+  const { origin } = req.headers;
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+
+  if (ALLOWED_CORS.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', '*');
+  }
+
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+
+  next();
 };
