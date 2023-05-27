5277f08395ef47ae792a938f38adb38be6fc4bb6
Eugeniy Razveev <razveev.evg@gmail.com>
2023-05-27 10:50:31 +0300

    test

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -116,7 +116,7 @@ function App() {
   function handleCardLike(card) {
     console.log('currentUser._id =>', currentUser._id);
     console.log('likes array =>', card.likes);
-    const isLiked = card.likes.some((item) => console.log(item._id));
+    const isLiked = card.likes.some((item) => console.log(item));
     console.log('isLiked =>', isLiked);
     api.changeLikeCardStatus(card._id, isLiked)
       .then((newCards) => {
