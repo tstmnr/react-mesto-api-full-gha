08383977d285db4de486f44c93588e85c8f9bce6
Eugeniy Razveev <razveev.evg@gmail.com>
2023-05-27 10:55:28 +0300

    card owner

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -114,10 +114,7 @@ function App() {
   }
 
   function handleCardLike(card) {
-    console.log('currentUser._id =>', currentUser._id);
-    console.log('likes array =>', card.likes);
     const isLiked = card.likes.some((item) => item === currentUser._id);
-    console.log('isLiked =>', isLiked);
     api.changeLikeCardStatus(card._id, isLiked)
       .then((newCards) => {
         setCards((state) => state.map((c) => c._id === card._id ? newCards : c));
